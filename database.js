c6dd53484bbd57a88e02b68781ed20acc152d575
John15j <83560505+John15j@users.noreply.github.com>
2026-06-06 12:34:37 -0400

    Update database.js

--- database.js
+++ database.js
@@ -1,8 +1,55 @@
+const Database = {
 
-const DB={
-init(){
-if(!Storage.get('cases').length){
-Storage.set('cases',[])
-}
-}
-}
+    init() {
+
+        const defaults = {
+
+            cases: [],
+            defendants: [],
+            officers: [],
+            hearings: [],
+            activity: []
+
+        };
+
+        Object.keys(defaults)
+            .forEach(key => {
+
+                if (
+                    !localStorage.getItem(key)
+                ) {
+
+                    Storage.set(
+                        key,
+                        defaults[key]
+                    );
+
+                }
+
+            });
+
+    },
+
+    log(message) {
+
+        const activity =
+            Storage.get("activity");
+
+        activity.unshift({
+
+            message,
+
+            date:
+                new Date()
+                .toLocaleString()
+
+        });
+
+        Storage.set(
+            "activity",
+            activity
+        );
+
+    }
+
+};
